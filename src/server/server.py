a75d7c04520f42798fd721f99c3413f84dd5683a
ThirdBlood <M1150562056@outlook.com>
2022-04-16 13:41:54 +0800

    修改不合适的变量名

--- src/server/server.py
+++ src/server/server.py
@@ -9,7 +9,7 @@ import sys
 
 '''
 变量备忘录
-users: 
+user_connections: 
     第一层索引：列表中的每一个元素都是列表，列表中存储着每个在线用户的信息
     第二层索引：
         0: 用户名
@@ -24,19 +24,19 @@ ip = settings.ip_address
 port = settings.network_port
 
 messages = queue.Queue()
-users = []  # 0:userName 1:connection
+user_connections = []  # 0:userName 1:connection
 lock = threading.Lock()
 
 
 def OnOnline():
     online = []
-    for new_index in range(len(users)):
-        online.append(users[new_index][0])
+    for new_index in range(len(user_connections)):
+        online.append(user_connections[new_index][0])
     return online
 
 
 class Server(threading.Thread):
-    global users, lock
+    global user_connections, lock
 
     def __init__(self):  # 构造函数
         threading.Thread.__init__(self)
@@ -54,11 +54,11 @@ class Server(threading.Thread):
             user = address[0] + ':' + str(address[1])
         tag = 1
         temp = user
-        for i in range(len(users)):  # 检验重名，则在重名用户后加数字
-            if users[i][0] == user:
+        for i in range(len(user_connections)):  # 检验重名，则在重名用户后加数字
+            if user_connections[i][0] == user:
                 tag = tag + 1
                 user = temp + str(tag)
-        users.append((user, conn))
+        user_connections.append((user, conn))
         online_users = OnOnline()
         self.Load(online_users, address)
         # 在获取用户名后便会不断地接受用户端发来的消息（即聊天内容），结束后关闭连接。
@@ -71,9 +71,9 @@ class Server(threading.Thread):
         except Exception as e:
             print(e)
             j = 0  # 用户断开连接
-            for man in users:
+            for man in user_connections:
                 if man[0] == user:
-                    users.pop(j)  # 服务器段删除退出的用户
+                    user_connections.pop(j)  # 服务器段删除退出的用户
                     break
                 j = j + 1
 
@@ -98,17 +98,17 @@ class Server(threading.Thread):
             if not messages.empty():
                 message = messages.get()
                 if isinstance(message[1], str):
-                    for i in range(len(users)):
+                    for i in range(len(user_connections)):
                         data = ' ' + message[1]
-                        users[i][1].send(data.encode('utf-8'))
+                        user_connections[i][1].send(data.encode('utf-8'))
                         print(data)
                         print('\n')
 
                 if isinstance(message[1], list):
                     data = json.dumps(message[1])
-                    for i in range(len(users)):
+                    for i in range(len(user_connections)):
                         try:
-                            users[i][1].send(data.encode('utf-8'))
+                            user_connections[i][1].send(data.encode('utf-8'))
                         except Exception as e:
                             print(e)
 
