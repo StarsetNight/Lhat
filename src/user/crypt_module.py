7f1d9749425d8c054eb30ee0fe7987998cacd502
ThirdBlood <M1150562056@outlook.com>
2022-04-20 10:27:26 +0800

    加密功能不成功，回档

--- src/user/crypt_module.py
+++ src/user/crypt_module.py
@@ -14,3 +14,17 @@ def decrypt(string, privkey):
     解密函数（文本）
     """
     return rsa.decrypt(string, privkey).decode('utf-8')
+
+
+def loadPublicKey(key: str):
+    """
+    加载公钥
+    """
+    return rsa.PublicKey.load_pkcs1(key)
+
+
+def loadPrivateKey(key: str):
+    """
+    加载私钥
+    """
+    return rsa.PrivateKey.load_pkcs1(key)
