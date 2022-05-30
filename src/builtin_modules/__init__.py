2ce3fa095c460bded1a605d3dbca3c1e669491ca
ThirdBlood <M1150562056@outlook.com>
2022-05-30 08:28:05 +0800

    合并，增强耦合性

--- src/builtin_modules/__init__.py
+++ src/builtin_modules/__init__.py
@@ -1,13 +1,15 @@
 import socket
-import threading
-import sys
 import webbrowser
+import os.path
+import re
+import json
+import sys
 import time
+import threading
 
 # 记得改chatwindow和loginwindow里面的图片资源导入路径再打包
 
 from . import Doc
-from . import chat_operations as chatops
 
 from PySide6.QtWidgets import QApplication, QMainWindow, QStyleFactory, QMessageBox, QDialog
 
@@ -19,13 +21,15 @@ from .ui.Signal import chat_window_signal
 from .ui.Signal import login_window_signal
 from .ui.Signal import register_window_signal
 
-server_ip = chatops.ip  # 定义服务器IP
-server_port = chatops.port  # 定义服务器端口
-username = chatops.user  # 定义用户名
-online_users = chatops.users  # 定义在线用户列表
-chat_with = chatops.default_chat  # 定义聊天对象，默认为群聊
-chatting_rooms = chatops.chatting_rooms  # 定义自己所在的聊天室列表
-logable = chatops.logable  # 定义是否可以记录日志
+server_ip = ''
+server_port = ''
+username = ''
+textbox = ''  # 用于显示在线用户的列表框
+show = 1  # 用于判断是开还是关闭列表框
+users = []  # 在线用户列表
+default_chat = ''  # 聊天对象，先定义为空，因为不同的服务端，需要的聊天对象不同
+chatting_rooms = []  # 自己所在的聊天室列表
+logable = False  # 定义是否可以记录日志
 
 
 class LoginApplication(QMainWindow):
@@ -51,8 +55,7 @@ class LoginApplication(QMainWindow):
         """
         global server_ip, server_port, username
         try:
-            server_ip, server_port = self.ui.input_box_server_ip_port.toPlainText().split(
-                ':')  # 获取服务器IP和端口
+            server_ip, server_port = self.ui.input_box_server_ip_port.toPlainText().split(':')  # 获取服务器IP和端口
         except ValueError:  # 如果输入的不是IP:端口的格式，则报错
             QMessageBox.warning(
                 self, "警告", '请输入正确的服务器地址格式：\n<IP地址> : <外部端口>', QMessageBox.Yes, QMessageBox.Yes)
@@ -142,11 +145,9 @@ class ChatApplication(QMainWindow):
             self.backLoginWindow()
             return
         if username:
-            self.connection.send(chatops.pack(
-                username + ' ' * (256 - len(username.encode('utf-8'))), None, None, 'USER_NAME'))  # 发送用户名
+            self.connection.send(self.pack(username, None, None, 'USER_NAME'))  # 发送用户名
         else:
-            self.connection.send(chatops.pack(
-                '用户名不存在  ', None, None, 'USER_NAME'))  # 发送用户名
+            self.connection.send(self.pack('用户名不存在', None, None, 'USER_NAME'))  # 发送用户名
             username = server_ip + ':' + server_port
 
         self.startReceive()  # 创建线程用于接收消息
@@ -179,12 +180,6 @@ class ChatApplication(QMainWindow):
         def setOnlineUser(msg: str):
             self.ui.output_box_online_user.setText(msg)
 
-        def savePubKey(key: str):
-            self.public_key = key
-
-        def savePrivateKey(key: str):
-            self.private_key = key
-
         chat_window_signal.appendOutPutBox.connect(appendOutPut)
         chat_window_signal.setOutPutBox.connect(setOutPut)
         chat_window_signal.clearOutPutBox.connect(clearOutPut)
@@ -197,19 +192,14 @@ class ChatApplication(QMainWindow):
         chat_window_signal.setOnlineUserList.connect(setOnlineUser)
         chat_window_signal.clearOnlineUserList.connect(clearOnlineUser)
 
-        chat_window_signal.saveRsaPublicKey.connect(savePubKey)
-        chat_window_signal.saveRsaPrivateKey.connect(savePrivateKey)
-
     def sendMessage(self):
         raw_message = self.ui.input_box_message.toPlainText()
-        chatops.send(self.connection, raw_message, username,
-                     chat_window_signal.appendOutPutBox)
+        self.send(raw_message)
         chat_window_signal.clearInPutBox.emit()  # 清空输入框
 
     def startReceive(self):
         # 你懂的，这是一个线程，用于接收消息，函数呢？在模块里面的函数，可以直接调用，但是要加模块名
-        self.receive_thread = threading.Thread(target=chatops.receive,
-                                               args=(self, chat_window_signal))
+        self.receive_thread = threading.Thread(target=self.receive)
         self.receive_thread.start()  # 开始线程接收信息
 
     def triggeredMenubar(self, triggeres):
@@ -246,6 +236,226 @@ class ChatApplication(QMainWindow):
         else:
             self.ui.input_box_message.setFocus()
             return
+
+    # ==================================================================================================================
+    # 四大函数开始
+
+    @staticmethod
+    def pack(raw_message: str, send_from, chat_with, message_type, file_name=None):
+        """
+        打包消息，用于发送
+        :param raw_message: 正文消息
+        :param send_from: 发送者
+        :param chat_with: 聊天对象
+        :param message_type: 消息类型
+        :param file_name: 文件名，如果不是文件类型，则为None
+        """
+        message = {
+            'by': send_from,
+            'to': chat_with,
+            'type': message_type,
+            'time': time.time(),
+            'message': raw_message,
+            'file': file_name,
+        }  # 先把收集到的信息存储到字典里
+        return json.dumps(message).encode('utf-8')  # 再用json打包
+
+    @staticmethod
+    def unpack(json_message: str):
+        """
+        解包消息，用于接收JSON格式的消息
+        看不懂message字典对应的东西吗？message_types里面有。
+
+        :param json_message: JSON消息
+        :return: 返回的东西有很多，也有可能是报错
+        ==================================================
+        return返回值大全：
+        JSON_MESSAGE_NOT_EOF: 消息不完整
+        NOT_JSON_MESSAGE: 不是JSON格式的消息
+        MANIFEST_NOT_JSON: 用户名单不是JSON格式
+        UNKNOWN_MESSAGE_TYPE: 未知消息类型
+        --------------------------------------------------
+        FILE_SAVED: 文件保存成功
+        <一个列表>: 这是用户名单，有用的
+        """
+        by_color = 'blue'
+        try:
+            message = json.loads(json_message)  # JSON加载
+        except json.decoder.JSONDecodeError:  # 如果加载失败，两种可能，第一种，长消息，第二种，断了。
+            return 'NOT_JSON_MESSAGE', json_message
+
+        if message['type'] == 'TEXT_MESSAGE' or message['type'] == 'COLOR_MESSAGE':  # 如果是纯文本消息
+            message_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(message['time']))  # 将时间戳转成日期时间
+            if message['by'] == 'Server':
+                by_color = 'red'
+            message_body = re.sub('&', '&amp;', message['message'])  # 将&替换成&amp;
+            message_body = re.sub(r'\t', '&nbsp;&nbsp;&nbsp;&nbsp;', message_body)  # 替换tab符
+            if message['type'] == 'TEXT_MESSAGE':
+                message_body = re.sub('<', '&lt;', message_body)  # 替换<
+                message_body = re.sub('>', '&gt;', message_body)  # 替换>
+            message_body = re.sub(' ', '&nbsp;', message_body)  # 替换空格
+            message_body = re.sub(r'\n', '<br/>', message_body)  # 替换换行符
+            message_body = f"<font color='{by_color}'>{message['by']}</font> <font color='grey'>[{message_time}]" \
+                           f"</font> : <br/>&nbsp;&nbsp;{message_body}"
+            return message['type'], message['to'], message_body, message['by']
+        elif message['type'] == 'USER_MANIFEST' or \
+                message['type'] == 'ROOM_MANIFEST' or \
+                message['type'] == 'MANAGER_LIST':  # 如果是用户列表
+            try:
+                manifest = json.loads(message['message'])  # 将用户列表转成列表
+                return message['type'], manifest
+            except json.decoder.JSONDecodeError:  # 如果转换失败，则返回错误
+                return 'MANIFEST_NOT_JSON'  # 用户名单不是JSON格式
+        elif message['type'] == 'DEFAULT_ROOM':
+            return message['type'], message['message']
+        else:
+            return 'UNKNOWN_MESSAGE_TYPE'
+
+    def send(self, raw_message: str):
+        """
+        发送消息。
+        """
+        chat_with = default_chat
+        color = False
+        if not raw_message.strip():  # 如果消息为空，则不发送，strip的作用是去掉首尾空格
+            self.chat_window_signal.appendOutPutBox.emit('[提示] 发送的消息不能为空！<br/>')
+            return  # 因为无法发送空消息，所以直接返回
+        elif raw_message.startswith('//tell '):  # 如果是私聊
+            if sys.getsizeof(raw_message) <= 968:  # 经过计算，1024个字节的消息以968个字节为正文差不多可以。
+                command_message = raw_message.split(' ')  # 分割完之后，分辨一下是否为命令
+                chat_with = command_message[1]
+                raw_message = re.sub('^//tell', '[私聊消息] 到', raw_message)  # 命令转正文，使用正则替换
+            else:
+                self.chat_window_signal.appendOutPutBox.emit('[提示] 发送的私聊消息长度不能大于968字节！<br/>'
+                                                             '&nbsp;&nbsp;建议不要大于300个汉字或900个英文字母和数字！')  # 私聊消息不能超过1024个字节
+        elif raw_message.startswith('//color '):  # 如果是彩色消息
+            command_message = raw_message.split(' ')
+            raw_message = re.sub('^//color .* ', f'<font color={" ".join(command_message[1:])}>', raw_message)
+            raw_message += '</font>'
+            color = True
+        elif raw_message.startswith('//help'):  # 如果是帮助请求
+            os.system('start notepad help.txt')
+            return
+        elif raw_message.startswith('//'):  # 如果是命令
+            raw_message = re.sub('^//', '', raw_message)
+            message = self.pack(raw_message, username, None, 'COMMAND')
+            self.connection.send(message)
+            time.sleep(0.05)
+            return
+        if not color:
+            message = self.pack(raw_message, username, chat_with, 'TEXT_MESSAGE')
+        else:
+            message = self.pack(raw_message, username, chat_with, 'COLOR_MESSAGE')
+        # 发送消息直到发送完毕
+        self.connection.sendall(message)
+        time.sleep(0.05)
+
+    def receive(self):
+        """
+        接收消息，但是得要TCP连接。
+        """
+        global default_chat, chatting_rooms  # 这个要引用的是全局变量
+        # received_long_data = ''
+        if os.path.exists(f'chat_{self.server_address}.txt'):
+            print('已找到聊天记录文件，正在读取旧服务器聊天记录……')
+            threading.Thread(target=self.read_record).start()
+        else:
+            self.chat_window_signal.appendOutPutBox.emit('哒哒！欢迎来到Lhat聊天室！大家开始聊天吧！<br/>'
+                                                         '更多操作提示请输入 //help 并发送！<br/>')
+
+        while True:
+            try:
+                received_data = self.connection.recv(1024)  # 接收信息
+            except ConnectionError:  # 如果与服务器断开连接
+                self.chat_window_signal.appendOutPutBox.emit(
+                    f'<font color="red">[严重错误] 呜……看起来与服务器断开了连接，服务姬正在努力修复呢……</font><br/>'
+                    f'主人可以试试断开连接并重新登录哦！<br/>')
+                self.log('与服务器断开了连接。')
+                return
+            if not received_data:  # 如果接收到的数据为空，则说明服务器已经关闭
+                self.chat_window_signal.appendOutPutBox.emit(
+                    f'<font color="red">[严重错误] 呜……看起来与服务器断开了连接，服务姬正在努力修复呢……</font><br/>'
+                    f'主人可以试试断开连接并重新登录哦！<br/>')
+                self.log('与服务器断开了连接。')
+                return
+            received_data = received_data.decode('utf-8')
+            print(received_data)  # ---
+            # if received_long_data:  # 如果有长消息，则尝试读取长消息
+            #     message = unpack(received_long_data)  # 解包消息
+            # else:
+            #     message = unpack(received_data)  # 解包消息
+            message = self.unpack(received_data)  # 解包消息
+            message_type = message[0]
+            if message_type == 'TEXT_MESSAGE' or message_type == 'COLOR_MESSAGE':  # 如果是文本消息
+                message_body = message[2]
+                self.chat_window_signal.appendOutPutBox.emit(message_body + '<br/>')
+                with open(f'chat_{self.server_address}.txt', 'a', encoding='utf-8') as chat_file:
+                    chat_file.write(received_data + '\n')
+                # received_long_data = ''  # 正常解包之后，清空长消息
+
+            elif message_type == 'USER_MANIFEST':
+                message_body = message[1]
+                online_users = message_body
+                self.chat_window_signal.clearOnlineUserList.emit()
+                self.chat_window_signal.appendOnlineUserList.emit(default_chat)
+                self.chat_window_signal.appendOnlineUserList.emit('<font color="#3333FF">====在线用户====</font>')
+                for user_index, online_username in enumerate(online_users):
+                    # online_username是用于显示在线用户的，不要与username混淆
+                    self.chat_window_signal.appendOnlineUserList.emit(str(online_username))
+                # received_long_data = ''  # 正常解包之后，清空长消息
+
+            elif message_type == 'MANAGER_LIST':
+                if message[1]:
+                    self.chat_window_signal.appendOutPutBox.emit('在线的维护者有：<br/>')
+                    for manager_index, manager_username in enumerate(message[1]):
+                        self.chat_window_signal.appendOutPutBox.emit(f'{manager_index + 1} {manager_username}<br/>')
+                else:
+                    self.chat_window_signal.appendOutPutBox.emit('暂无在线的维护者！<br/>')
+
+            elif message_type == 'ROOM_MANIFEST':
+                chatting_rooms = message[1]
+                self.log(f'聊天室列表更新为：{chatting_rooms}')
+
+            # elif message_type == 'FILE_RECV_DATA':
+            # file_name = message[1]
+            # file_data = message[2]
+            # signals.appendOutPutBox.emit('[文件] 锵锵！正在接收文件！<br/>')
+            # with open(file_name, 'ab') as chat_file:
+            # if isinstance(file_data, str):
+            # chat_file.write(file_data.encode('utf-8'))
+            # else:
+            # chat_file.write(file_data)
+            # signals.appendOutPutBox.emit('[文件] 锵锵！文件已接收！<br/>')
+            # received_long_data = ''  # 正常解包之后，清空长消息
+
+            elif message_type == 'NOT_JSON_MESSAGE':
+                self.log(f'服务端发来的消息不是JSON格式，消息内容为：{received_data}')
+
+            elif message_type == 'DEFAULT_ROOM':
+                default_chat = message[1]
+                self.chat_window_signal.appendOutPutBox.emit(
+                    f'[提示] 锵锵！已分配至默认聊天室：<font color="blue">{default_chat}</font><br/>')
+                self.log(f'已分配至默认聊天室：{default_chat}')
+
+    # ==================================================================================================================
+    # 四大函数结束
+
+    def read_record(self):
+        """
+        读取聊天记录，这个不算入四大函数，因为这是Lhat专有的。
+        """
+        with open(f'chat_{self.server_address}.txt', 'r', encoding='utf-8') as f:
+            data = 'RECORD READ START'
+            while data:
+                data = f.readline().strip()
+                message = self.unpack(data)  # 解包消息
+                try:
+                    message_body = message[2]
+                except IndexError:
+                    continue
+                self.chat_window_signal.appendOutPutBox.emit(message_body + '<br/>')
+        self.chat_window_signal.appendOutPutBox.emit('哒哒！欢迎来到Lhat聊天室！大家开始聊天吧！<br/>'
+                                                     '更多操作提示请输入 //help 并发送！<br/>')
 
     @staticmethod
     def log(content: str, end='\n', show_time=True):
