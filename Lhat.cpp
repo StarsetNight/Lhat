ed4d771dbba17b226957f16b8458d4e8518268d6
AdvancedK_ <M1150562056@outlook.com>
2023-11-14 21:39:02 +0800

    [+] 状态控件（用户列表）更新与适应性改进

--- Lhat.cpp
+++ Lhat.cpp
@@ -5,5 +5,6 @@ int main(int argc, char* argv[])
 	QApplication app(argc, argv); //Qt应用程序对象
 	ChatApplication *lhatwindow = new ChatApplication; //创建登录窗口
 	lhatwindow->show();
+	lhatwindow->onConnect(); //软件启动后打开登录窗口
 	return app.exec(); //主循环
 }
